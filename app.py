12a9508bab29ba14628a7feec4a94eea8edc7ad7
Greatcard11 <cardstels@gmail.com>
2026-06-02 07:11:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,6 @@ with col2:
 
 st.markdown('<div class="main-title">HR/ADMIN MANAGEMENT SYSTEM</div>', unsafe_allow_html=True)
 st.markdown('<div class="sub-title">Smart HR Operations & Employee Analytics Platform</div>', unsafe_allow_html=True)
-st.write("DEBUG SECRETS:", st.secrets)
 
 # =========================================
 # APP LINKS & HUB PLATFORM
